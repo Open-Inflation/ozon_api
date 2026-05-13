f027b793d3e14be669aaab5549642c1b2d64be9a
Miskler <69055489+Miskler@users.noreply.github.com>
2026-05-13 23:31:09 +0300

    бесконечный парс

--- example.py
+++ example.py
@@ -1,30 +1,34 @@
 from __future__ import annotations
 
 import asyncio
-import sys
-
-
-if hasattr(sys.stdout, "reconfigure"):
-    try:
-        sys.stdout.reconfigure(encoding="utf-8", errors="replace")
-    except Exception:
-        pass
-
 from ozon_api.abstraction import OzonPage
 from ozon_api.manager import OzonAPI
 
 
 async def main() -> None:
-    async with OzonAPI(headless=False) as api:
-        resp = await api.Catalog.feed()
-        model = OzonPage.model_validate(resp.json())
+    async def parse(model):
         products = model.extract_products()
 
         if not products:
             print("Товары не найдены.")
             return
 
-        print(products[0].render())
+        for product in products:
+            print(product.render())
+            
+    async with OzonAPI(headless=False) as api:
+        resp = await api.Catalog.feed()
+        with open("response.json", "w", encoding="utf-8") as f:
+            import json
+            f.write(json.dumps(resp.json(), indent=4, ensure_ascii=False))
+        model = OzonPage.model_validate(resp.json())
+        await parse(model)
+        for _i in range(100):
+            await asyncio.sleep(1)
+            resp = await api.Catalog.feed(page_url=model.next_page)
+            model = OzonPage.model_validate(resp.json())
+            await parse(model)
+
 
 
 if __name__ == "__main__":
